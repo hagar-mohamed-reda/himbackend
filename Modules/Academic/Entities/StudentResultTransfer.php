54421f9818c9dfcb1b03f46be37c3f35825998c0
mtareq340 <mtareq340@gmail.com>
2022-09-17 14:50:21 +0200

    tarekstage

--- Modules/Academic/Entities/StudentResultTransfer.php
+++ Modules/Academic/Entities/StudentResultTransfer.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Academic\Entities;
 use Modules\Account\Entities\AccountSetting;
+use Modules\Account\Entities\StudentOldBalance;
+use Modules\Account\Entities\Installment;
 use Modules\Settings\Entities\Level;
 
 class StudentResultTransfer extends Student
@@ -27,7 +29,7 @@ class StudentResultTransfer extends Student
         $registerHours = $this->getActualRegisterCourses()->sum('credit_hour');
         
         $valid = true;
-        if ($this->level_id < $lastLevel) {
+        if ($this->level_id < $lastLevel && $this->case_constraint_id != 1) {
             $nextLevel = $this->level_id + 1;
             $level = Level::find($nextLevel);
             
@@ -36,23 +38,37 @@ class StudentResultTransfer extends Student
                 $this->failedTransferReason = "شروط الطالب غير مطابقة للمستوى {".$level->name."} مطلوب {".$level->required_course."} مقرر و {".$level->required_hour."} ساعة";
             }
             
-            if ($nextLevel == $lastLevel && $this->getStudentBalance()->getCurrentBalanceTotal() > 0) {
-                $valid = false;
-                $this->failedTransferReason .= ", الطالب لم يقوم بدفع المصاريف الدراسية للتخرج";
-            }
+            // if ($nextLevel == $lastLevel && $this->getStudentBalance()->getCurrentBalanceTotal() > 0) {
+            //     $valid = false;
+            //     $this->failedTransferReason .= ", الطالب لم يقوم بدفع المصاريف الدراسية للتخرج";
+            // }
             
-            if ($term->id == 2 && $valid == false) {
-                $this->update([
-                    "case_constraint_id" => 3
-                ]);
-            } 
+            /* for term 2  */
+             if ($this->getStudentBalance()->getCurrentBalanceTotal() > 0) {
+                 $installments = Installment::where('student_id',$this->id)
+                                                ->where('paid',0)->delete();
+                 $studentOldBalance = StudentOldBalance::create([
+                        'student_id' => $this->id,
+                        'value' => $this->getStudentBalance()->getCurrentBalanceTotal(),
+                        'notes' => 'رسوم سابقة',
+                     ]);
+            }
+           
+            
+            /* if term is summer. */
+            
+            // if ($term->id == 2 && $valid == false) {
+            //     $this->update([
+            //         "case_constraint_id" => 3
+            //     ]);
+            // } 
             
             if ($valid == true) {
                 $this->update([
                     "level_id" => $nextLevel
                 ]);
             }
-        }
+        }else{}
         
         return $valid;
     }
